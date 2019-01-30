36b0e6a9f51887fc1ead1e4c3118d78a2fb1b159
Jan Bölsche <regular.gonzales@googlemail.com>
2019-01-30 12:11:03 +0100

    Don't re-render sheet of schema has not changed

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const h = require('mutant/html-element')
 const Value = require('mutant/value')
+const computed = require('mutant/computed')
 const getProperties = require('get-properties-from-schema')
 const pointer = require('json8-pointer')
+const comparer = require('deep-equal')
 
 module.exports = function(opts) {
   opts = opts || {}
 
   return function renderPropertySheet(kv, ctx) {
     ctx = ctx || {}
-    const contentObs = ctx.contentObs || Value({})
-    const mergedContent = kv.value.content
-    const schema = mergedContent.schema
-    if (!schema) return
-
-    const skvs = getProperties(schema)
-    return h('fieldset.tre-property-sheet', {
-      disabled: ctx.disabled == true
-    }, skvs.map(skv => renderProperty(skv, [])))
+    const content = kv && kv.value.content
+    const contentObs = ctx.contentObs || Value(content || {})
+    const schema = computed(contentObs, content => content.schema, {comparer})
+    
+    return computed(schema, schema => {
+      if (!schema) return []
+      const skvs = getProperties(schema)
+      return h('fieldset.tre-property-sheet', {
+        disabled: ctx.disabled == true
+      }, skvs.map(skv => renderProperty(skv, [])))
+    })
 
     function renderProperty(skv, path) {
       const fullPath = path.concat([skv.key])
@@ -24,65 +28,53 @@ module.exports = function(opts) {
       if (skv.value.type == 'object') {
         return h('details', {open: true}, [
           h('summary', title),
-          h('div.properties', 
+          h('div.properties', [
             getProperties(skv.value).map(skv => renderProperty(skv, fullPath))
-          )
+          ])
         ])
       }
-      let value = pointer.find(mergedContent, fullPath)
-      const isInherited = value !== pointer.find(contentObs(), fullPath)
-      
-      function save(v) {
-        if (v == value) return
-        const coerce = {
-          number: Number,
-          integer: Number,
-          boolean: Boolean
-        }[skv.value.type] || (x => x)
-        value = coerce(v)
-        // avoids destroying event.target in event handler
-        setTimeout( ()=> {
-          const c = contentObs()
-          const parent = pointer.find(c, path) || createContainer(schema, c, path)
-          parent[skv.key] = value
-          contentObs.set(c)
-        }, 0)
-      }
-      if ('number integer string'.split(' ').includes(skv.value.type)) {
-        return [
-          h('div.property', {
-            classList: isInherited ?  ['inherited'] : [],
-            attributes: {
-              'data-schema-type': skv.value.type,
-              'data-schema-name': skv.key,
-              'data-schema-path': pointer.encode(fullPath)
-            }
-          }, [
-            h('span', title),
-            h('input', {
-              type: skv.value['input-type'] || (skv.value.type == 'number' ? 'number' : 'text'),
-              value,
-              'ev-blur': e => {
-                save(e.target.value)
-              },
-              'ev-change': e => {
-                save(e.target.value)
-              },
-              'ev-keydown': e => {
-                if (e.key == 'Escape') {
-                  e.blur()
-                  e.preventDefault()
-                }
-                if (e.key == 'Enter') {
-                  save(e.target.value)
-                  e.preventDefault()
-                }
+
+      return computed(contentObs, content => {
+        let value = pointer.find(content, fullPath)
+        const isInherited = value !== pointer.find(content, fullPath)
+        const save = saveFunc(value, contentObs)
+
+        if ('number integer string'.split(' ').includes(skv.value.type)) {
+          return [
+            h('div.property', {
+              classList: isInherited ?  ['inherited'] : [],
+              attributes: {
+                'data-schema-type': skv.value.type,
+                'data-schema-name': skv.key,
+                'data-schema-path': pointer.encode(fullPath)
               }
-            })
-          ])
-        ]
-      }
-      return []
+            }, [
+              h('span', title),
+              h('input', {
+                type: skv.value['input-type'] || (skv.value.type == 'number' ? 'number' : 'text'),
+                value,
+                'ev-blur': e => {
+                  save(e.target.value)
+                },
+                'ev-change': e => {
+                  save(e.target.value)
+                },
+                'ev-keydown': e => {
+                  if (e.key == 'Escape') {
+                    e.blur()
+                    e.preventDefault()
+                  }
+                  if (e.key == 'Enter') {
+                    save(e.target.value)
+                    e.preventDefault()
+                  }
+                }
+              })
+            ])
+          ]
+        }
+        return []
+      })
     }
   }
 }
@@ -129,4 +121,23 @@ function createContainer(schema, obj, path) {
   if (!newObj) throw new Error('Unable to create container. Wrong schema type: ' + type)
   obj[key] = newObj
   return createContainer(prop.value, obj[key], path)
+}
+
+function saveFunc(value, contentObs) {
+  return function(v) {
+    if (v == value) return
+    const coerce = {
+      number: Number,
+      integer: Number,
+      boolean: Boolean
+    }[skv.value.type] || (x => x)
+    value = coerce(v)
+    // avoids destroying event.target in event handler
+    setTimeout( ()=> {
+      const c = contentObs()
+      const parent = pointer.find(c, path) || createContainer(schema, c, path)
+      parent[skv.key] = value
+      contentObs.set(c)
+    }, 0)
+  }
 }
